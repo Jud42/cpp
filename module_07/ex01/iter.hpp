930dcda60e45c05bb65e0ddcdb65c483bb645fb4
Jud42 <judiismals@gmail.com>
2023-03-24 19:21:12 +0100

    mod-07 ex-02 just to do: manage exception

---
+++ module_07/ex01/iter.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmamison <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 08:42:42 by rmamison          #+#    #+#             */
+/*   Updated: 2023/03/24 11:49:28 by rmamison         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _ITER_HPP_
+#define _ITER_HPP_
+
+#include <iostream>
+
+template < typename T>
+void printContent(T & a)
+{
+	std::cout << "contenu: " << a << std::endl;
+}
+
+//Tfunc will be a pointer to function
+template < typename T1 , typename Tfunc >
+void iter(T1 *array, int size, Tfunc funct)
+{
+	for (int i = 0; i < size; i++)
+		funct(array[i]);
+}
+
+
+#endif
---
+++ module_07/ex01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmamison <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 07:40:28 by rmamison          #+#    #+#             */
+/*   Updated: 2023/03/24 11:52:08 by rmamison         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+int main( void ) {
+
+	//int
+	std::cout << "******INT******" << std::endl;
+	int array[] = {28, 03, 1993};
+	iter(array, 3, &printContent< int >);
+
+	//string
+	std::cout << "\n******STRING******" << std::endl;
+	std::string array1[] = {"TEST", "STRING", "CONTENT"};
+	iter(array1, 3, &printContent< std::string >);
+
+	return 0;
+}
